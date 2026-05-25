e8e1e463d1947cd7cdf47b620f88f3923515afce
prince kumar <princekumar1821006@gmail.com>
2026-05-25 18:47:02 +0530

    Update Signup.js

--- frontend2/src/pages/Signup.js
+++ frontend2/src/pages/Signup.js
@@ -27,7 +27,7 @@ function Signup() {
             return handleError('name, email and password are required')
         }
         try {
-            const url = `https://mongodb-vrzy.vercel.app//auth/login`;
+            const url = `https://mongodb-vrzy.vercel.app/auth/login`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
