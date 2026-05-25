1a7a9cf05b16e820e93a0e29970c7ae2f812eec2
prince kumar <princekumar1821006@gmail.com>
2026-05-25 18:46:46 +0530

    Update Login.js

--- frontend2/src/pages/Login.js
+++ frontend2/src/pages/Login.js
@@ -27,7 +27,7 @@ function Login() {
             return handleError('email and password are required')
         }
         try {
-            const url = `https://mongodb-vrzy.vercel.app//auth/login`;
+            const url = `https://mongodb-vrzy.vercel.app/auth/login`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
