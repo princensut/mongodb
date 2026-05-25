b4dcc358f2854c9b6b0f911d033cdbd9e6cc3a82
princekumar1821006-skraper <princekumar1821006@gmail.com>
2026-05-25 14:06:45 +0530

    fix vercel backend structure

---
+++ backend/api/index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('../config/db');
+const cors = require('cors');
+
+const app = express();
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', require('../routes/auth'));
+
+module.exports = app;
